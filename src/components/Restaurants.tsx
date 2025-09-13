5d6de55619dd7aedfbf7726762f3a9eedcc099d0
sammy2201 <bhavanish2000@gmail.com>
2025-09-13 12:16:33 +0530

    filtering is done

--- src/components/Restaurants.tsx
+++ src/components/Restaurants.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { RefObject } from "react";
+import { RefObject, useState, useMemo } from "react";
 import { useRouter } from "next/navigation";
 import "../css/components/Restaurants.css";
-import { Heading1, Star } from "lucide-react";
+import { Star } from "lucide-react";
 
 export default function Restaurants({
   restaurants,
@@ -19,21 +19,57 @@ export default function Restaurants({
   hasMore: boolean;
 }) {
   const router = useRouter();
+  const [filter, setFilter] = useState("");
 
   const handleClick = (placeId: string) => {
     router.push(`/restaurant/${placeId}`);
   };
+
+  const filteredRestaurants = useMemo(() => {
+    let result = [...restaurants];
+
+    console.log("Applying filter:", restaurants);
+    switch (filter) {
+      case "rating-asc":
+        result.sort((a, b) => (a.rating || 0) - (b.rating || 0));
+        break;
+      case "rating-desc":
+        result.sort((a, b) => (b.rating || 0) - (a.rating || 0));
+        break;
+      case "bar":
+        result = result.filter((r) => r.types?.includes("bar"));
+        break;
+      case "open-now":
+        result = result.filter((r) => r.opening_hours?.open_now == true);
+        break;
+    }
+
+    return result;
+  }, [restaurants, filter]);
 
   return (
     <div className="restaurants-list">
       <h2>Restaurants</h2>
       <div
         ref={containerRef}
-        className={restaurants.length == 0 ? " container" : " blur container "}
+        className={restaurants.length == 0 ? "container" : "blur container"}
         onScroll={handleScroll}>
-        <div className="filters"></div>
+        <div className="filters">
+          <h3>List Of Restaurants</h3>
+          <select
+            className="filter-dropdown"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}>
+            <option value="">Filter by...</option>
+            <option value="rating-asc">Rating: Low → High</option>
+            <option value="rating-desc">Rating: High → Low</option>
+            <option value="open-now">Open Now</option>
+            <option value="bar">Bar</option>
+          </select>
+        </div>
+
         <div className="grid">
-          {restaurants.map((r, idx) => (
+          {filteredRestaurants.map((r, idx) => (
             <div
               key={`${r.place_id}-${idx}`}
               className="card"
@@ -49,6 +85,7 @@ export default function Restaurants({
             </div>
           ))}
         </div>
+
         {loading && <p className="center-text">Loading...</p>}
         {!hasMore && !loading && (
           <p className="center-text">No more restaurants</p>
